1d8d25f4f3bb25c462b5191ec46ed8d73c3f6ef6
Dimitry <xxhellfire69xx@gmail.com>
2019-11-14 16:28:40 +0100

    coal

---
+++ VR_Workshop_2018.2.20f1 - light/Assets/Scripts/ScriptsTest/InstanceCoal.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InstanceCoal : MonoBehaviour {
+
+    public GameObject _coal;
+    public int _max = 20;
+    private int _currentCoal;
+
+    private void Update()
+    {
+        Quaternion _cone = new Quaternion(0, Random.Range(0, 360), Random.Range(0, 30), 0);
+        if (_currentCoal < _max)
+        {
+            GameObject _coalChild = Instantiate(_coal, transform.position, _cone);
+            _currentCoal++;
+        }
+    }
+}
