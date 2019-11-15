203ea6ee685fa5f9a770ebf4bc4a12d2da5810ea
Dimitry <xxhellfire69xx@gmail.com>
2019-11-15 13:15:49 +0100

    backupCoal

--- VR_Workshop_2018.2.20f1 - light/Assets/Scripts/ScriptsTest/InstanceCoal.cs
+++ VR_Workshop_2018.2.20f1 - light/Assets/Scripts/ScriptsTest/InstanceCoal.cs
@@ -10,11 +10,21 @@ public class InstanceCoal : MonoBehaviour {
 
     private void Update()
     {
-        Quaternion _cone = new Quaternion(0, Random.Range(0, 360), Random.Range(0, 30), 0);
-        if (_currentCoal < _max)
+        //Quaternion _cone = new Quaternion(0, Random.Range(0, 360), Random.Range(0, 30), 0);
+        //if (_currentCoal < _max)
+        //{
+        //    GameObject _coalChild = Instantiate(_coal, transform.position, _cone);
+        //    _currentCoal++;
+        //    _coalChild.transform.parent = gameObject.transform;
+        //}
+
+        
+    }
+    private void OnTriggerEnter(Collider col)
+    {
+        if(col.transform.tag == "Shovel")
         {
-            GameObject _coalChild = Instantiate(_coal, transform.position, _cone);
-            _currentCoal++;
+
         }
     }
 }
